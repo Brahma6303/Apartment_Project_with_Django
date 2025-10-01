524345192d9ddb78c42779968a8bb9a5a5185b23
kanna Brahmaiah <brahmaiahkanna8@gmail.com>
2025-10-01 14:37:23 +0530

    Adding more fields in form with using usercreation model and add one more model

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CustomUser
+from .models import CustomUser,UserProfile
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
@@ -7,11 +7,15 @@ class UserRegistrationForm(UserCreationForm):
     first_name=forms.CharField(required=True,max_length=30,label='First Name*')
     last_name=forms.CharField(required=True,max_length=30,label='Last Name*')
     email=forms.EmailField(required=True,max_length=30,label='Email*')
+    age=forms.IntegerField(required=True,max_value=100,label='Age*')
+    mobile_number=forms.CharField(required=True,max_length=10,label='Mobile Number*')
+    city=forms.CharField(required=True,max_length=30,label='City*')
+    country=forms.CharField(required=True,max_length=30,label='Country*')
     
     
     class Meta:
         model=CustomUser
-        fields=['first_name','last_name','email']
+        fields=['first_name','last_name','age','mobile_number','email','city','country']
         widgets={'password':forms.PasswordInput()}
 
     def clean_email(self):
@@ -23,6 +27,12 @@ class UserRegistrationForm(UserCreationForm):
     def save(self):
         user =super().save(commit=False)
         user.save()
+        profile=UserProfile(age=self.cleaned_data['age'],
+                            mobile_number=self.cleaned_data['mobile_number'],
+                            city=self.cleaned_data['city'],
+                            country=self.cleaned_data['country'],
+                            user=user)
+        profile.save()
         return user
         
         
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
@@ -8,3 +9,12 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['first_name','last_name']
     
+class UserProfile(models.Model):
+    age=models.SmallIntegerField()
+    mobile_number=models.CharField(max_length=10,default='')
+    city=models.CharField(max_length=30,default='')
+    country=models.CharField(max_length=30,default='')
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} {self.user.email} ({self.mobile_number}) {self.city} , {self.country})"
